b2aba109bd185855ec2fd2ad5a47b167de765af7
brkyaydn97 <56759314+brkyaydn97@users.noreply.github.com>
2021-05-06 23:33:11 +0300

    Delete BaseCustomerManager.java

--- BaseCustomerManager.java
+++
@@ -1,5 +0,0 @@
-public abstract class BaseCustomerManager implements ICustomerService{
-    public void save(Customer customer){
-        System.out.println("Saved to DB:"+customer.getFirstName());
-    }
-}
