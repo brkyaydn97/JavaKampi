5c23aa05765f6dbeae62fd60624be785d9e4cdcd
brkyaydn97 <56759314+brkyaydn97@users.noreply.github.com>
2021-05-06 23:33:53 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-BaseCustomerManager customerManager=new NeroCustomerManager();
-customerManager.save(new Customer(1,"Berkay","Aydın",1997,1234123412));
-
-
-    }
-}
